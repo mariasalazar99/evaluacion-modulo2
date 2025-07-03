da07cd03522d98054395ca11a2368b211d035e82
mariasalazar99 <mariateresalazar99@gmail.com>
2025-07-03 00:15:41 -0400

    Correcciones y se añade JS en Amenazas Comunes

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Scroll de Home //
 document.addEventListener('DOMContentLoaded', () => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -9,6 +10,22 @@ document.addEventListener('DOMContentLoaded', () => {
   }, { threshold: 0.1 });
 
   document.querySelectorAll('.fade-in-section').forEach(el => {
+    observer.observe(el);
+  });
+});
+
+// Scroll de "Amenazas Comunes" //
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); // evita reanimar
+      }
+    });
+  }, { threshold: 0.1 });
+
+  document.querySelectorAll(".fade-in-on-scroll").forEach(el => {
     observer.observe(el);
   });
 });
